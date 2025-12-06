e792a09d45431867a4cced5899eecf2b604f5de8
ankitkvishwakarma <ankit789.en@gmail.com>
2025-12-06 23:47:35 +0530

    fixed

--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-<<<<<<< HEAD
-  baseURL: import.meta.env.VITE_BACKEND_URL + "/api", 
-=======
   baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
->>>>>>> 7bd45fd (some bug fixed)
   withCredentials: true,
 });
 
